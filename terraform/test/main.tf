986971115063bd50c08e666af4a0583634cc37b1
Alaxay <alexey@Alaxays-MacBook-Air.local>
2026-02-17 22:54:59 +0300

    commit from macbook

--- terraform/test/main.tf
+++ terraform/test/main.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-
+  region = "us-east-1"
 }
 
 resource "aws_key_pair" "my_auth" {
-key_name = "my_ssh_key"
-public_key = file("~/.ssh/id_rsa.pub")
+  key_name   = "my_ssh_key"
+  public_key = file(pathexpand("~/.ssh/id_rsa.pub"))
 }
 
 resource "aws_instance" "my_amazon" {
-count         = 5
-ami           = "ami-0532be01f26a3de55"
-instance_type = "t3.micro"
-key_name      = aws_key_pair.my_auth.key_name
-vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-tags = {
+  count                  = 5
+  ami                    = "ami-0532be01f26a3de55"
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.my_auth.key_name
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags = {
     Name = "test-amazon${count.index}"
   }
 }
@@ -36,6 +36,19 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+locals {
+  repo_root      = abspath("${path.module}/../..")
+  inventory_path = "${local.repo_root}/ansible/test-amazon-inventory.yaml"
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = local.inventory_path
+
+  content = templatefile("${path.module}/inventory.tmpl", {
+    all_public_ips = aws_instance.my_amazon[*].public_ip
+  })
+}
+
 
 output "all_public_ips" {
   value = aws_instance.my_amazon[*].public_ip
